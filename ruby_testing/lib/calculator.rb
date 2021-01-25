9f101c39fe08eae5f641fe880318f2fff04b0b6a
Jebitok <sharonkosgei4@gmail.com>
2021-01-25 13:51:01 +0300

    add methods & test

--- ruby_testing/lib/calculator.rb
+++ ruby_testing/lib/calculator.rb
@@ -2,4 +2,16 @@ class Calculator
     def add(a,b)
         a + b
     end
+    def add(a,b,c)
+        a + b + c
+    end
+    def multiply(a,b)
+        a * b
+    end
+    def subtract(a,b)
+        a - b
+    end
+    def divide(a,b)
+        a / b
+    end
 end
--- ruby_testing/spec/calculator_spec.rb
+++ ruby_testing/spec/calculator_spec.rb
@@ -9,4 +9,16 @@ describe Calculator do
           expect(calculator.add(2, 5, 7)).to eql(14)
         end
     end
+    describe "#multiply" do
+      it "returns the product of two numbers" do
+      end
+    end
+    describe "#subtract" do
+      it "returns the minus of two numbers" do
+      end
+    end
+    describe "#divide" do
+      it "returns the division of two numbers" do
+      end
+    end
 end
